5bf60e0e92d836930e0b52b2fe50c5c0c6d48a24
njkf <43401909+njkf@users.noreply.github.com>
2019-03-04 13:57:48 +0100

    Add files via upload

--- Javascript.js
+++ Javascript.js
@@ -380,81 +380,252 @@
 //   }
 // }
 
-const canvas = document.querySelector('canvas');
-const ctx = canvas.getContext('2d');
+// const canvas = document.querySelector('canvas');
+// const ctx = canvas.getContext('2d');
 
-const getCoordinatesInCanvas = (canvasEl, event) => {
-  const rect = canvasEl.getBoundingClientRect(); // récupérer la taille de <canvas> et sa position relative par rapport à la zone d'affichage
-  const x = event.clientX - rect.left;
-  const y = event.clientY - rect.top;
-  console.log(`x: ${x}, y: ${y}`);
+// const getCoordinatesInCanvas = (canvasEl, event) => {
+//   const rect = canvasEl.getBoundingClientRect(); // récupérer la taille de <canvas> et sa position relative par rapport à la zone d'affichage
+//   const x = event.clientX - rect.left;
+//   const y = event.clientY - rect.top;
+//   console.log(`x: ${x}, y: ${y}`);
+// }
+// canvas.addEventListener('click', (event) => {
+//   getCoordinatesInCanvas(canvas, event);
+// })
+
+// ctx.strokeStyle = '#fff';
+// ctx.fillStyle = '#fff';
+
+// // HEAD
+// ctx.beginPath();
+// ctx.arc(200, 180, 60, 0, 2 * Math.PI);
+// ctx.stroke();
+// ctx.fill();
+
+// // BODY
+// ctx.beginPath();
+// ctx.arc(200, 360, 120, 0, 2 * Math.PI);
+// ctx.stroke();
+// ctx.fill();
+
+// // HAT
+// ctx.strokeStyle = '#000';
+// ctx.fillStyle = '#000';
+
+// ctx.fillRect(123, 130, 150, 30); // bas 
+// ctx.fillRect(152, 47, 90, 100); // haut
+
+// // LEFT ARM
+// ctx.lineWidth = 5;
+// ctx.beginPath();
+// ctx.moveTo(132, 301);
+// ctx.lineTo(30, 254);
+// ctx.stroke();
+
+// // LEFT ARM
+// ctx.lineWidth = 5;
+// ctx.beginPath();
+// ctx.moveTo(256, 305);
+// ctx.lineTo(330, 267);
+// ctx.lineTo(370, 210);
+// ctx.stroke();
+
+// // EYES
+// ctx.beginPath();
+// ctx.arc(177, 177, 5, 0, 2 * Math.PI);
+// ctx.stroke();
+// ctx.fill();
+
+// ctx.beginPath();
+// ctx.arc(220, 177, 5, 0, 2 * Math.PI);
+// ctx.stroke();
+// ctx.fill();
+
+// // NOSE
+// ctx.strokeStyle = 'orange';
+// ctx.fillStyle = 'orange';
+// ctx.beginPath();
+// ctx.moveTo(200, 186);
+// ctx.lineTo(124, 210);
+// ctx.lineTo(199, 210);
+// ctx.quadraticCurveTo(203, 195, 199, 186)
+// ctx.stroke();
+// ctx.fill();
+
+// // SCARF
+// ctx.strokeStyle = 'red';
+// ctx.fillStyle = 'red';
+
+// ctx.fillRect(157, 230, 90, 20); // bas 
+// ctx.fillRect(216, 243, 20, 150); // haut
+
+// Logguez dans la console 'Hello' 3 secondes après le rendu de la page.
+setTimeout(() => {
+  console.log('Hello');
+}, 3000)
+
+// Logguez dans la console 'Logged after 5 seconds' 5 secondes après le rendu de la page.
+setTimeout(() => {
+  console.log('Logged after 5 seconds');
+}, 5000)
+
+// Ajoutez un écouteur d'événement 'click' à l'objet window pour logguer dans la console 'Clicked' 3 secondes après le clique.
+winbdow.addEventListener('click', () => {
+  setTimeout(() => {
+    console.log('Cliked!');
+  }, 3000);
+});
+
+// Créez un div. Au survol de ce div logguez dans la console 'I was hovered 5.5 seconds ago' 5 secondes plus tard.
+const div = document.querySelector('');
+dropZone.addEventListener('mouseover', () => changeDropZoneState(true));
+
+// Créez dynamiquement une image avec src égal à http://lorempixel.com/400/200/.
+//  Quand l'image sera entièrement chargée, logguez dans la console 'Done!' et l'affichez sur la page.
+// En cas d'erreur de chargement, logguez dans la console 'Error'.
+const myImg = document.createElement('img');
+myImg.src = 'http://lorempixel.com/400/200/';
+myImg.onload = () =>{
+  console.log('Done!');
+  document.body.appendChild(myImg)
 }
-canvas.addEventListener('click', (event) => {
-  getCoordinatesInCanvas(canvas, event);
+myImg.onerror = () =>{
+  console.log('Error');
+}
+
+// Créez dynamiquement une node de script avec l'url de la librarie lodash: https://cdn.jsdelivr.net/npm/lodash@4.17.11/lodash.min.js.
+// Quand le script sera téléchargé, appelez la fonction round pour arrondir le nombre 10.02.
+const myScript = document.createElement('script');
+myScript.src = 'https://cdn.jsdelivr.net/npm/lodash@4.17.11/lodash.min.js.';
+myScript.onload = () =>{
+  console.log('Done!');
+}
+document.body.appendChild(myScript)
+myScript.onerror = () =>{
+  console.log('Error');
+}
+
+// Récupérez l'information du profil de Han Solo (son id dans la base de données est 14). Ensuite affichez sur votre page ces données: nom, sexe, date de naissance, couleur des yeux, couleur des cheveux.
+let xhttp = new XMLHttpRequest();
+xhttp.onreadystatechange = () => {
+  if(whttp.readyState == 4 && xhttp.status == 200){
+    const rawValue = xhttp.responseText;
+    const parsedValue = JSON.parse(rawValue);
+
+    const hanSoloInfo = document.createElement('div');
+    hanSoloInfo.innerHTML = `
+      <p>Nom: ${parsedValue.name}</p>
+      <p>Sexe: ${parsedValue.gender}</p>
+      <p>Date de naissance: ${parsedValue.birth_year}</p>
+    `
+    document.body.appendChild(hanSoloInfo);
+  }
+};
+xhttp.open('GET', 'https://swapi.co/api/people/14/', true);
+xhttp.send();
+
+// Trouvez toutes les personnes avec le nom de famille Skywalker. Affichez leur profiles sur votre page.
+let xhttp = new XMLHttpRequest();
+xhttp.onreadystatechange = () => {
+  if(whttp2.readyState == 4 && whttp2.status == 200) {
+    console.log(xhttp2.responseText)
+    const rawValue = xhttp2.responseText;
+    const parsedValue = JSON.parse(rawValue);
+
+    parsedfValue.results.forEach(person => {
+      const parsonDiv = document.createElement('div');
+      personDiv.innerHTML = `
+        <hr>
+        <p>Nom: ${person.name}</p>
+        <p>Sexe: ${person.gender}</p>
+        <p>Date de naissance: ${person.birth_year}</p>
+      `
+      document.body?appendChild(personDiv);
+    })
+  }
+};
+xhttp2.open('GET', 'https://swapi.co/api/people/?search=skywalker', true);
+xhttp2.send();
+
+// Récupérez et affichez les données de 5 planètes avec les ids suivants: 
+const planets = [1, 2, 3, 4, 5];
+planets.forEach(pnaletId => {
+  let xhttp = new XMLHttpRequest();
+  xhttp.onredaystatechange = () => {
+    if (xhttp.readyState == 4 && xhttp.status == 200) {
+     console.log(xhttp.responseText)
+    }
+  };
+  xhttp.open('GET', 'https://swapi.co/api/planets/'+planetId, true);
+  xhttp.send();
 })
 
-ctx.strokeStyle = '#fff';
-ctx.fillStyle = '#fff';
 
-// HEAD
-ctx.beginPath();
-ctx.arc(200, 180, 60, 0, 2 * Math.PI);
-ctx.stroke();
-ctx.fill();
+// Créez une promise (promesse) qui retourne une chaîne de caractères 'Hello' immediatement. Récupérez et logguez la valeur qu'elle retourne.
+constmyPromise = new Promise((resolve, reject) => {
+  if (5 > 3) {
+    setTimeout(() => {
+      resolve('Hello');
+    }
+  } else {
+    reject('Error');
+  }
+});
 
-// BODY
-ctx.beginPath();
-ctx.arc(200, 360, 120, 0, 2 * Math.PI);
-ctx.stroke();
-ctx.fill();
+mypromise
+  .then(result => console.log(result))
+  .catch(error => console.log(error));
 
-// HAT
-ctx.strokeStyle = '#000';
-ctx.fillStyle = '#000';
+// Créez une promise qui retourne un nombre N. Enchaînez le traitement de la promise pour effectuez les operations suivantes:
+// multiplier N par 2
+// ensuite soustraire 1
+// ensuite decrementer de 2
+// ensuite augmenter de 10. Logguez le résultat réçu.
+const getNum = new Promise((resolve, reject) => {
+  resolve(10);
+});
+getNum
+  .then(num => {
+    return num * 2;
+  })
+  .then(num => {
+    return num - 1;
+  })
+  .then(res => {
+    console.log('res', res);
+  })
+  .catch(err => {
+    console.log('err', err);
+  })
 
-ctx.fillRect(123, 130, 150, 30); // bas 
-ctx.fillRect(152, 47, 90, 100); // haut
 
-// LEFT ARM
-ctx.lineWidth = 5;
-ctx.beginPath();
-ctx.moveTo(132, 301);
-ctx.lineTo(30, 254);
-ctx.stroke();
+// Placeholder API: https://jsonplaceholder.typicode.com/ Récupérez et affichez un article avec l'id 25, ensuite récupérez et affichez ses commentaires.
+fetch('https://jsonplaceorder.typicode.com/posts/25')
+  .then(reponse => reponse.json())
+  .then(result=> {
+    console.log('result', result);
+  })
 
-// LEFT ARM
-ctx.lineWidth = 5;
-ctx.beginPath();
-ctx.moveTo(256, 305);
-ctx.lineTo(330, 267);
-ctx.lineTo(370, 210);
-ctx.stroke();
+// Utilisez l'API de https://reqres.in/ et la méthode fetch qui retourne une promise. Récupérez et affichez les informations de l'utilisateur avec l'id 5.
+fetch('https://reqres.in/5')
+  .then(reponse => reponse.json())
+  .then(result=> {
+    console.log('result', result);
+  })
 
-// EYES
-ctx.beginPath();
-ctx.arc(177, 177, 5, 0, 2 * Math.PI);
-ctx.stroke();
-ctx.fill();
+// Créez un formulaire d'inscription. A la soumission du formulaire envoyez une requête pour créez un nouvel utilisateur. Sauvegardez ces données dans les cookies.
+const signupData = {
+  email: 'test@ynov.com',
+  password: 'azerty'
+  lastname: 'Toto'
+}
+fetch('https://reqres.in/api/register', {
+  method: 'POST',
+  body: JSON.stringify(signupDate),
+  hedaers: {
+    'Content-Type': 'application/json'
+  }
+})
+  .then(response => response.json())
+  .then(result => console.log('result', result));
 
-ctx.beginPath();
-ctx.arc(220, 177, 5, 0, 2 * Math.PI);
-ctx.stroke();
-ctx.fill();
-
-// NOSE
-ctx.strokeStyle = 'orange';
-ctx.fillStyle = 'orange';
-ctx.beginPath();
-ctx.moveTo(200, 186);
-ctx.lineTo(124, 210);
-ctx.lineTo(199, 210);
-ctx.quadraticCurveTo(203, 195, 199, 186)
-ctx.stroke();
-ctx.fill();
-
-// SCARF
-ctx.strokeStyle = 'red';
-ctx.fillStyle = 'red';
-
-ctx.fillRect(157, 230, 90, 20); // bas 
-ctx.fillRect(216, 243, 20, 150); // haut
